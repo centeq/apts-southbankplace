afa14a326df1cfdda4828dd5b28fe682697eb9a2
marett <brendan@centralequity.com.au>
2015-09-28 17:14:26 +1000

    Changed some things from red to white and added sass.

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head> 
+    <link href="css/style.css" rel="stylesheet" />
+    </head>
+    <body  >
+    <h1>Hello </h1>
+        
+        <div class="div1">
+        hello
+            <div class="div2">hi</div>
+        </div>               
+        <div class="div2">jdj</div>
+    </body>
+
+</html>
